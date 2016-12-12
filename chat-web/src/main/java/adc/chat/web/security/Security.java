92b64789499aacc0444f4d80b6be60d5cc566c14
Antoine de Chassey <antoinedechassey@gmail.com>
2016-12-12 11:17:14 -0500

    Fix XSS injection

--- chat-web/src/main/java/adc/chat/web/security/Security.java
+++ chat-web/src/main/java/adc/chat/web/security/Security.java
@@ -2,21 +2,35 @@ package adc.chat.web.security;
 
 import java.util.regex.Pattern;
 
+import org.unbescape.html.HtmlEscape;
+
 import adc.chat.web.dto.Message;
 
 public class Security {
 
 	public static Message sanitizeMessage(Message message) {
-		Pattern p = Pattern.compile("<script[^>]*>(.*?)</script>", Pattern.DOTALL | Pattern.CASE_INSENSITIVE);
-		String error = "Message has been deleted for security reasons.";
+//		String error = "Message has been deleted for security reasons.";
+
+		// Pattern p = Pattern.compile("<script[^>]*>(.*?)</script>",
+		// Pattern.DOTALL | Pattern.CASE_INSENSITIVE);
+		// if (message.getMessage() != null)
+		// message.setMessage(p.matcher(message.getMessage()).replaceAll(error));
+		// if (message.getType() != null)
+		// message.setType(p.matcher(message.getType()).replaceAll(error));
+		// if (message.getPseudo() != null)
+		// message.setPseudo(p.matcher(message.getPseudo()).replaceAll(error));
+		// if (message.getDate() != null)
+		// message.setDate(p.matcher(message.getDate()).replaceAll(error));
+
 		if (message.getMessage() != null)
-			message.setMessage(p.matcher(message.getMessage()).replaceAll(error));
+			message.setMessage(HtmlEscape.escapeHtml5(message.getMessage()));
 		if (message.getType() != null)
-			message.setType(p.matcher(message.getType()).replaceAll(error));
+			message.setType(HtmlEscape.escapeHtml5(message.getType()));
 		if (message.getPseudo() != null)
-			message.setPseudo(p.matcher(message.getPseudo()).replaceAll(error));
+			message.setPseudo(HtmlEscape.escapeHtml5(message.getPseudo()));
 		if (message.getDate() != null)
-			message.setDate(p.matcher(message.getDate()).replaceAll(error));
+			message.setDate(HtmlEscape.escapeHtml5(message.getDate()));
+
 		return message;
 
 		// final Pattern pattern =
